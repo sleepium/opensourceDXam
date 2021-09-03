0098d6eafc569e726bfcbdc07a6a51ff67c09964
sleepium <32081179+sleepium@users.noreply.github.com>
2021-09-03 16:32:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en" style="background-color:#242735">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>SLP Dashboard | Virtual Domain</title>
+  <meta name="description" content="SLP Dashboard">
+  <meta name="author" content="Sleepium">
+
+  <meta property="og:title" content="SLP Dashboard | Virtual Domain">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="slpdashboard.virtualdomain.dx.am">
+  <meta property="og:description" content="SLP Dashboard Embedded https://berrycakes.github.io/SLPticker/">
+  <!--meta property="og:image" content="image.png"-->
+
+  <link rel="icon" href="/favicon.ico">
+  <!--link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png"-->
+
+  <!--link rel="stylesheet" href="css/styles.css?v=1.0"-->
+</head>
+
+<body>
+<div style="text-align:center; margin:0 auto; width:100%;">
+<div style="text-align:center; margin:0 auto; width:100%;">
+        <!--embed src="https://berrycakes.github.io/SLPticker/" style="width:100%; height:100%" /-->
+  <!--script src="js/scripts.js"></script-->
+  <div class="nomics-ticker-widget" data-name="Smooth Love Potion" data-base="SLP" data-quote="PHP" style="width:100%; height:100%; max-width:100%; margin:0 auto;"></div>
+  
+  </div>
+  </div>
+  <div style="text-align:center; font-size:50px; font-weight:bold; color:white;">My Dream Team</div>
+  <a style="text-decoration:none; color:orange;" href="https://marketplace.axieinfinity.com/axie/2186269" target="_blank">Axie #2186269 | Aquatic</a><br />
+  <a style="text-decoration:none; color:orange;" href="https://marketplace.axieinfinity.com/axie/1937312" target="_blank">Axie #1937312 | Bug</a><br />
+  <a style="text-decoration:none; color:orange;" href="https://marketplace.axieinfinity.com/axie/2328767" target="_blank">Axie #2328767 | Plant</a><br />
+  
+  <script src="https://widget.nomics.com/embed.js"></script>
+</body>
+</html>
